a506575310a895430374eab8f1190a7953d78dde
chanhday03 <lethitrang010603@gmail>
2023-02-19 16:37:31 +0700

    responsive home section with touch slider using swiper.js

--- app.js
+++ app.js
@@ -15,3 +15,4 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+// slideshow
